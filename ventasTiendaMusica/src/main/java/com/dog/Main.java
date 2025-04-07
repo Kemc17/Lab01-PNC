4107b491b0d5c8244dc98670cb61b2c52ca84a15
alvarado356 <jesus.alvarado@silverlinecentralamerica.com>
2025-04-06 18:35:40 -0600

    Añadiendo ultimas configuraciones

--- ventasTiendaMusica/src/main/java/com/dog/Main.java
+++ ventasTiendaMusica/src/main/java/com/dog/Main.java
@@ -6,21 +6,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static List<Artista> artistas = new ArrayList<>();
+    static List<Disco> discos = new ArrayList<>();
     public static void main(String[] args)
     {
-
-        System.out.println("Bienvenido al sistema de gestión de artistas y discos.");
-        System.out.println("1. Agregar artista");
-        System.out.println("2. Agregar disco");
-        System.out.println("3. Calcular artista más discos vendido");
-        System.out.println("4. Mostrar disco con más de X vendido");
-        System.out.println("5. Vender disco");
-
-        Scanner scanner = new Scanner(System.in);
-        List<Artista> artistas = new ArrayList<>();
-        List<Disco> discos = new ArrayList<>();
-
         while (true) {
+            System.out.println("Bienvenido al sistema de gestión de artistas y discos.");
+            System.out.println("1. Agregar artista");
+            System.out.println("2. Agregar disco");
+            System.out.println("3. Calcular artista más discos vendido");
+            System.out.println("4. Mostrar disco con más de X vendido");
+            System.out.println("5. Vender disco");
             System.out.print("Seleccione una opción: ");
             int opcion = scanner.nextInt();
             scanner.nextLine();
@@ -41,9 +38,7 @@ public class Main {
                     discos.add(new Disco(artista, titulo, 0));
                     break;
                 case 3:
-                    Artista artistaMasDiscosVendidos = artistas.stream()
-                            .max(Comparator.comparing(Artista::getVentasTotales))
-                            .orElse(null);
+                    Artista artistaMasDiscosVendidos = artistaMasBuscado(artistas);
                     if (artistaMasDiscosVendidos != null) {
                         System.out.println("El artista con más discos vendidos es: " + artistaMasDiscosVendidos.getNombre());
                     } else {
@@ -55,30 +50,43 @@ public class Main {
                     int minVentas = scanner.nextInt();
                     scanner.nextLine();
                     discos.stream()
-                            .filter(disco -> (disco.getVentasTotales()) > minVentas)
+                            .filter(disco -> (disco.getVentasTotales()) >= minVentas)
                             .forEach(disco -> System.out.println("Disco: " + disco.getTitulo() + ", Ventas: " + disco.getVentasTotales()));
                     break;
                 case 5:
-                    System.out.print("Ingrese el título del disco a vender: ");
-                    String tituloDisco = scanner.nextLine();
-                    Disco discoAVender = discos.stream()
-                            .filter(disco -> disco.getTitulo().equals(tituloDisco))
-                            .findFirst()
-                            .orElse(null);
-                    if (discoAVender != null) {
-                        System.out.print("Ingrese la cantidad de ventas: ");
-                        int ventas = scanner.nextInt();
-                        scanner.nextLine();
-                        int nuevasVentas = (discoAVender.getVentasTotales()) + ventas;
-                        discoAVender.setVentasTotales(Integer.valueOf(nuevasVentas));
-                        System.out.println("Ventas actualizadas para el disco " + tituloDisco + ": " + nuevasVentas);
-                    } else {
-                        System.out.println("Disco no encontrado.");
-                    }
+                    ventaProducto();
                     break;
                 default:
                     System.out.println("Opción no válida. Intente de nuevo.");
             }
+        }
+    }
+    public static Artista artistaMasBuscado (List<Artista> artistas){
+        return artistas.stream()
+                .max(Comparator.comparing(Artista::getVentasTotales))
+                .orElse(null);
+    }
+    public static void ventaProducto(){
+        System.out.print("Ingrese el título del disco a vender: ");
+        String tituloDisco = scanner.nextLine();
+        Disco discoAVender = discos.stream()
+                .filter(disco -> disco.getTitulo().equals(tituloDisco))
+                .findFirst()
+                .orElse(null);
+        if (discoAVender != null) {
+            System.out.print("Ingrese la cantidad de ventas: ");
+            int ventas = scanner.nextInt();
+            scanner.nextLine();
+            discoAVender.setVentasTotales(discoAVender.getVentasTotales() + ventas);
+            System.out.println("Ventas actualizadas para el disco " + tituloDisco + ": " + discoAVender.getVentasTotales());
+            System.out.println(discoAVender.toString());
+            Artista artistaDeDisco = artistas.stream()
+                    .filter(artista -> artista.getId().equals(discoAVender.getArtista()))
+                    .findFirst()
+                    .orElse(null);
+            artistaDeDisco.setVentasTotales(artistaDeDisco.getVentasTotales() + ventas);
+        } else {
+            System.out.println("Disco no encontrado.");
         }
     }
 }
